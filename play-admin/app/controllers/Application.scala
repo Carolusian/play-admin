84c6bf862785afd9f7dcb92fceb942be2f7a5067
pkufashuo400 <pkufashuo400@gmail.com>
2013-07-27 00:02:07 +0800

    test

--- play-admin/app/controllers/Application.scala
+++
@@ -1,9 +0,0 @@
-package controllers.admin 
-
-import play.api.mvc._
-
-object Application extends Controller {
-  def index = Action { implicit request =>
-    Ok(views.html.admin.index("hello"))
-  }
-}
